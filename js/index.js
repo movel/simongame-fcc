ceee0f5037fd750bf925db274d2fb301d7c29c4d
C64\COMP64 <movel22@gmail.com>
2018-03-05 20:44:35 +0300

    add window.onload function

--- js/index.js
+++ js/index.js
@@ -1,15 +1,57 @@
 "use strict";
 
-let sound1 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-let sound2 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-let sound3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
-let sound4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+window.onload = function() {
+    let selector = '.slider';
+    let slider_toggle = document.querySelector(selector);
+    slider_toggle.addEventListener('mousedown', toggle);
 
-let sounds = new Array(4);
-sounds[0] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-sounds[1] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-sounds[2] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
-sounds[3] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+    let start = document.getElementById('start');
+
+    let toggled = false;
+
+    function addStartGameListener() {
+        start.addEventListener('mousedown', startGame);
+    }
+
+    function removeStartGameListener() {
+        start.removeEventListener('mousedown', startGame);
+    }
+
+    function startGame() {
+        // Game is Starting!!!
+        playAudio();
+    }
+
+    function toggle() {
+        if (!toggled) {
+            toggled = true;
+            toggleLed(true);
+            addStartGameListener();
+        } else {
+            toggled = false;
+            toggleLed(false);
+            removeStartGameListener();
+        }
+    }
+
+    function toggleLed(on) {
+        let countClass = document.getElementsByClassName('count');
+        let cls = "led-off";
+        if (on) countClass[0].classList.remove(cls);
+        else countClass[0].classList.add(cls);
+    }
+};
+
+// let sound1 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+// let sound2 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+// let sound3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+// let sound4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+// let sounds = new Array(4);
+// sounds[0] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+// sounds[1] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+// sounds[2] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+// sounds[3] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -28,51 +70,9 @@ function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
-// let x = document.getElementsByClassName('sound1');
-// console.log(x);
+let x = document.getElementsByClassName('sound1')[0];
 
 function playAudio() {
-    sounds[0].play();
-    // console.log(sound1);
-}
-
-let selector = '.slider';
-let slider_toggle = document.querySelector(selector);
-slider_toggle.addEventListener('mousedown', toggle);
-
-let start = document.getElementById('start');
-
-let toggled = false;
-
-function addStartGameListener() {
-    start.addEventListener('mousedown', startGame);
-}
-
-function removeStartGameListener() {
-    start.removeEventListener('mousedown', startGame);
-}
-
-function startGame() {
-    // console.log('Game is Starting!!!');
-    playAudio();
-}
-
-function toggle() {
-    if (!toggled) {
-        toggled = true;
-        toggleLed(true);
-        addStartGameListener();
-    } else {
-        toggled = false;
-        toggleLed(false);
-        removeStartGameListener();
-    }
-
-}
-
-function toggleLed(on) {
-    let countClass = document.getElementsByClassName('count');
-    let cls = "led-off";
-    if (on) countClass[0].classList.remove(cls);
-    else countClass[0].classList.add(cls);
+    x.currentTime = 0;
+    x.play();
 }
